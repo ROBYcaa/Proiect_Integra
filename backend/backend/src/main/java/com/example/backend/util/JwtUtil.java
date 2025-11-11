17e9d2d811e6f6e8780a99d1bcdbffde6eca5963
ROBYcaa <robertgindila4@gmail.com>
2025-11-11 19:16:01 +0200

    Added JWT dependencies+files

---
+++ backend/backend/src/main/java/com/example/backend/util/JwtUtil.java
@@ -0,0 +1,27 @@
+package com.example.backend.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private static final String SECRET_KEY =
+            "ThisIsASecretKeyForJWTWithAtLeast256BitsLength!";
+
+    public String generateToken(String userId, String role) {
+        return Jwts.builder()
+                .setHeaderParam("typ"
+                        ,
+                        "JWT")
+                .setSubject(userId)
+                .claim("role"
+                        , role)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
