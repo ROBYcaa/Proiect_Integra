761dcba4e8b08a6c8d25c95b3b5c2750104ae95e
ROBYcaa <robertgindila4@gmail.com>
2025-11-12 20:18:03 +0200

    tratament poate fi facut doar de doctor

--- backend/backend/src/main/java/com/example/backend/controller/DoctorController.java
+++ backend/backend/src/main/java/com/example/backend/controller/DoctorController.java
@@ -1,5 +1,7 @@
 package com.example.backend.controller;
 
+import com.example.backend.model.Treatment;
+import com.example.backend.service.TreatmentService;
 import com.example.backend.model.User;
 import com.example.backend.service.DoctorService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,9 +16,11 @@ import java.util.List;
 public class DoctorController {
 
     private final DoctorService doctorService;
+    private final TreatmentService treatmentService;
 
-    public DoctorController(DoctorService doctorService) {
+    public DoctorController(DoctorService doctorService, TreatmentService treatmentService) {
         this.doctorService = doctorService;
+        this.treatmentService = treatmentService;
     }
 
     @GetMapping("/patients")
@@ -30,5 +34,18 @@ public class DoctorController {
 
         List<User> patients = doctorService.getAllPatients();
         return ResponseEntity.ok(patients);
+    }
+
+    @PostMapping("/treatments")
+    public ResponseEntity<?> createTreatment(@RequestBody Treatment treatment,
+                                             HttpServletRequest request) {
+        String role = (String) request.getAttribute("role");
+
+        if (role == null || !"doctor".equalsIgnoreCase(role)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body("Acces interzis: doar doctorii pot crea tratamente.");
+        }
+        Treatment savedTreatment = treatmentService.createTreatment(treatment);
+        return ResponseEntity.ok(savedTreatment);
     }
 }
