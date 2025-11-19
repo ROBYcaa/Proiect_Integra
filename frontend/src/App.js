cf92a39d1b53955ec8247afbe1a6dedce024d68e
ROBYcaa <robertgindila4@gmail.com>
2025-11-19 10:06:52 +0200

    dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import './App.css';
+import Login from './login';
 import Dashboard from "./Dashboard";
 
 function App() {
   return (
-      <div className="App">
+      <div>
         <Dashboard />
       </div>
   );
