f1c938007f6604b28416943202cf7bee78b30c01
ROBYcaa <robertgindila4@gmail.com>
2026-01-29 11:34:34 +0200

    aplicatia se deschide in login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import {useEffect, useState} from "react";
-import NavBar from "./components/NavBar";
 import PrescriptionForm from "./components/prescriptionform/PrescriptionForm";
 import Treatments from "./components/treatments/Treatments";
 import Export from "./components/export/Export";
 import Chat from "./components/chat/Chat";
-
 
 function App() {
     const [user, setUser] = useState({
@@ -40,8 +38,8 @@ function App() {
 
     return (
         <BrowserRouter>
-            <NavBar user={user} onLogout={handleLogout} />
             <Routes>
+                <Route path="/" element={<Login setUser={setUser} />} />
                 <Route path="/login" element={<Login setUser={setUser}/>} />
                 <Route path="/dashboard" element={<Dashboard/>} />
                 <Route path="/prescribe" element={<PrescriptionForm />} />
